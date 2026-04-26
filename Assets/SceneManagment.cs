21b262727c979f4d47832ad5a916751867a01ba6
PolGames <company.polgames@mail.ru>
2026-04-26 13:18:19 +0600

    Add Sound for button and add partition

--- Assets/SceneManagment.cs
+++ Assets/SceneManagment.cs
@@ -1,15 +1,41 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneManagment : MonoBehaviour
 {
+    private AudioSource ButtonAudioSource;
+
+    private void Start()
+    {
+        ButtonAudioSource = GetComponent<AudioSource>();
+
+        if(SceneManager.GetActiveScene().name == "Prev")
+        {
+            StartCoroutine(WaitLoadSceneMenu(0));
+        }
+    }
+
     public void SceneLoader(int sceneIndex)
     {
-       SceneManager.LoadScene(sceneIndex); 
+       ButtonAudioSource.Play();
+        StartCoroutine(WaitButton(sceneIndex));
     }
 
     public void ExitApp()
     {
         Application.Quit();
+    }
+
+    private IEnumerator WaitLoadSceneMenu(int indexScene)
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(indexScene);
+    }
+
+    private IEnumerator WaitButton(int indexScene)
+    {
+        yield return new WaitForSeconds(0.3f);
+        SceneManager.LoadScene(indexScene);
     }
 }
